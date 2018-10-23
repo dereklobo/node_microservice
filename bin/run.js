db6cc8c9ee2d0a26a0b22704afbac7fa16062740
Roylin <dereklobo89@gmail.com>
2018-10-23 18:11:38 -0400

    connected to wit.ai

--- bin/run.js
+++ bin/run.js
@@ -5,10 +5,12 @@ const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 
+const witToken = process.env.witToken;
+const witClient = require('../server/witClient')(witToken);
 const slackToken = process.env.slackToken;
 const slackLogLevel = 'verbose';
 
-const rtm = slackClient.init(slackToken, slackLogLevel);
+const rtm = slackClient.init(slackToken, slackLogLevel,witClient);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
--- server/slackClient.js
+++ server/slackClient.js
@@ -4,17 +4,55 @@ const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 let rtm = null;
+let npl = null;
 
 function handleOnAuthenticated(rtmStartData) {
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 }
 
 function handleOnMessage(message) {
-    console.log(message);
 
-  rtm.sendMessage('this is a test message', message.channel, function messageSent() {
-    // optionally, you can supply a callback to execute once the message has been sent
-  });
+    if(message.text.toLowerCase().includes('iris')){
+        npl.ask(message.text,(err,res)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+
+            try{
+                if(!res.intent || !res.intent[0] || !res.intent[0].value){
+                    throw new Error("Could not extract intent");
+                }
+
+                const intent = require("../intent/" + res.intent[0].value + "Intent");
+
+                intent.process(res,function(error,response){
+                    if(error){
+                        console.log(error.message);
+                        return;
+                    }
+                    return rtm.sendMessage(response,message.channel);
+                });
+            }catch(err){
+                console.log(err);
+                console.log(res);
+                rtm.sendMessage("Sorry , I dont know what you are talking about !",message.channel);
+            }
+
+            if(!res.intent){
+                return rtm.sendMessage("Sorrz, I don't know what your talking about", message.channel);
+            }else if(res.intent[0].value =='time' && res.location){
+                return rtm.sendMessage(`I dont yet know the time in ${res.location[0].value}`,message.channel);
+            }else{
+                console.log(res);
+                return rtm.sendMessage("Sorrz, I don't know what your talking about", message.channel);
+            }
+    
+            rtm.sendMessage('Sorry, I did not understand', message.channel, function messageSent() {
+                // optionally, you can supply a callback to execute once the message has been sent
+            });
+        });
+    }
 }
 
 function addAuthenticatedHandler(rtm, handler) {
@@ -22,8 +60,9 @@ function addAuthenticatedHandler(rtm, handler) {
 }
 
 
-module.exports.init = function slackClient(token, logLevel) {
-    rtm = new RtmClient(token, {logLevel: logLevel});
+module.exports.init = function slackClient(token, logLevel,nplClient) {
+    rtm = new RtmClient(token, {logLevel: logLevel,useRtmConnect: true,dataStore: false,});
+    npl = nplClient;
     addAuthenticatedHandler(rtm, handleOnAuthenticated);
     rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
     return rtm;
