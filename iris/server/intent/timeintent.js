e0c4e44359919b95e1a62959a8d92850585550c5
Roylin <dereklobo89@gmail.com>
2018-10-23 23:24:25 -0400

    service registry

---
+++ iris/server/intent/timeintent.js
@@ -0,0 +1,26 @@
+'use strict';
+const request = require('superagent');
+
+module.exports.process = function process(intentData,registry,cb){
+    if(intentData.intent[0].value !=='time')
+        return cb(new Error(`Expected time intent, got ${intentData.intent[0].value}`));
+
+    if(!intentData.location)
+        return cb(new Error(`Missing location in the intent`));
+    
+
+    const location = intentData.location[0].value.replace(/,.?iris/i,'');
+
+    const service = registry.get('time');
+    if(!service) return cb(false,'No service available');
+
+    request.get(`http:\\${service.ip}:${service.port}/service/${location}`,(err,res)=>{
+        if(err||res.statusCode!=200||!res.body.result){
+            console.log(err);
+            // console.log(res.body);
+
+            return cb(false,`I had a problem finding a time in ${location}`)
+        }
+        return cb(false,`In ${location}, it is now ${res.body.result}`);
+    });
+}
